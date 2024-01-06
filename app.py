13441db1abc0407efc358ee613e0896224ae5875
Kaique Matheus R. Cunha <mcunha@cpqd.com.br>
2024-01-06 13:00:15 -0300

    Refatorando: pacote resources

--- app.py
+++ app.py
@@ -1,38 +1,10 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from resources.hotel import Hoteis
 
 app = Flask(__name__)
 api = Api(app)
 
-
-hoteis = [
-	{
-		'hotel_id': 'alpha',
-		'nome':'Alhpa Hotel',
-		'estrelas':4.3,
-		'diaria':420.34,
-		'cidade':'Rio de Janeiro'
-	},
-	{
-		'hotel_id': 'bravo',
-		'nome':'Bravo Hotel',
-		'estrelas':4.4,
-		'diaria':380.90,
-		'cidade':'Santa Catarina'
-	},
-	{
-		'hotel_id': 'charlie',
-		'nome':'Charlie Hotel',
-		'estrelas':4.5,
-		'diaria':450.90,
-		'cidade':'São Paulo'
-	}
-]
-
-class Hoteis(Resource):
-
-	def get(self):
-		return {'hoteis': hoteis}
 
 api.add_resource(Hoteis, '/hoteis')
 

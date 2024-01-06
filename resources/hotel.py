7e35ffd6c2d5812e73774940293679467a606548
Kaique Matheus R. Cunha <mcunha@cpqd.com.br>
2024-01-06 15:00:42 -0300

    Implementando GET, POST, PUT e DELETE
    Api-02

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
 	{
@@ -29,3 +29,52 @@ class Hoteis(Resource):
 
 	def get(self):
 		return {'hoteis': hoteis}
+	
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+	
+    def encontrar_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+    
+    def get(self, hotel_id):
+        hotel = Hotel.encontrar_hotel(hotel_id)
+        if hotel:
+             return hotel
+        return {'message': 'Hotel not found.'}, 404 # not found
+	
+    def post(self, hotel_id):
+        
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hoteis.append(novo_hotel)
+
+        return novo_hotel, 200
+
+    def put(self, hotel_id):
+
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.encontrar_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200 # ok
+    
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 # criado
+
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message':'Hotel deleted'}, 200
